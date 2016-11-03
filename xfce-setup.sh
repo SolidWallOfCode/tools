b4a8d6ac53f4f51206a6af5d0a5895b0efa502d6
Alan M. Carroll <solidwallofcode@yahoo-inc.com>
2016-11-03 09:09:42 -0500

    Python package tweaks.

--- xfce-setup.sh
+++ xfce-setup.sh
@@ -2,7 +2,9 @@
 dnf upgrade
 # For local networking, edit the connection to have a DHCP client id that
 # matches the hostname.
-dnf install gcc gcc-c++ autoconf automake libtool gdb bison flex openssl openssl-devel tcl-devel pcre-devel patch emacs wireshark-gnome libcap-devel git perl-ExtUtils-MakeMaker python-sphinx doxygen graphviz libxml2-devel gpg libffi-devel python-virtualenv python-lxml system-config-users nghttp2 libyaml-devel
+dnf install gcc gcc-c++ autoconf automake libtool gdb bison flex openssl openssl-devel tcl-devel pcre-devel patch emacs wireshark-gnome libcap-devel git perl-ExtUtils-MakeMaker python-sphinx doxygen graphviz libxml2-devel gpg libffi-devel python-devel python-virtualenv python-lxml system-config-users nghttp2 libyaml-devel redhat-rpm-config
+
+# redhat-rpm-config is for installing Python pacakges via PIP that need to be compiled.
  
 systemctl enable sshd.service
 systemctl start sshd.service
