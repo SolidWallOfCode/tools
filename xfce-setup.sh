436b40f1d2e53c0b4ea9bd8ce75bf650e22b3830
Alan M. Carroll <amc@apache.org>
2017-01-27 10:00:42 -0600

    Tweaks to xfce setup and changed git push default to "current".

--- xfce-setup.sh
+++ xfce-setup.sh
@@ -4,6 +4,9 @@ dnf upgrade
 # matches the hostname.
 dnf install gcc gcc-c++ autoconf automake libtool gdb bison flex openssl openssl-devel tcl-devel pcre-devel patch emacs wireshark-gnome libcap-devel git perl-ExtUtils-MakeMaker python-sphinx doxygen graphviz libxml2-devel gpg libffi-devel python-devel python-virtualenv python-lxml system-config-users nghttp2 libyaml-devel redhat-rpm-config
 
+pip install --upgrade pip
+pip install sphinx
+
 # redhat-rpm-config is for installing Python pacakges via PIP that need to be compiled.
  
 systemctl enable sshd.service
