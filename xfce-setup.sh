0a9ad6b5fecdb2070864ea2cd7efed3b12b12eda
Alan M. Carroll <amc@apache.org>
2017-03-04 14:06:57 -0600

    Remove unneeded sphix install. Using pip now.

--- xfce-setup.sh
+++ xfce-setup.sh
@@ -2,7 +2,7 @@
 dnf upgrade
 # For local networking, edit the connection to have a DHCP client id that
 # matches the hostname.
-dnf install gcc gcc-c++ autoconf automake libtool gdb bison flex openssl openssl-devel tcl-devel pcre-devel patch emacs wireshark-gnome libcap-devel git perl-ExtUtils-MakeMaker python-sphinx doxygen graphviz libxml2-devel gpg libffi-devel python-devel python-virtualenv python-lxml system-config-users nghttp2 libyaml-devel redhat-rpm-config jemalloc-devel
+dnf install gcc gcc-c++ autoconf automake libtool gdb bison flex openssl openssl-devel tcl-devel pcre-devel patch emacs wireshark-gnome libcap-devel git perl-ExtUtils-MakeMaker doxygen graphviz libxml2-devel gpg libffi-devel python-devel python-virtualenv python-lxml system-config-users nghttp2 libyaml-devel redhat-rpm-config jemalloc-devel
 
 pip install --upgrade pip
 pip install sphinx
