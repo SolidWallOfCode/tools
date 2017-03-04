53fd16502916e04205d9cad736aaee4a5afb35fb
Alan M. Carroll <amc@apache.org>
2017-03-04 14:12:04 -0600

    Fix ATS git setup script.

--- ats-utils/ats-git-setup.sh
+++ ats-utils/ats-git-setup.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 dir=${1:-ats}
-git clone https://github.com/apache/trafficserver.git ${dir}
-cd ${dir}
-git remote add swoc git@github.com:SolidWallOfCode/trafficserver.git
-git remote add asf https://git-dual.apache.org/repos/asf/trafficserver.git
+git clone git@github.com:apache/trafficserver.git
+if [ -d ${dir} ] ; then
+    cd ${dir}
+    git remote rename origin ats
+    git remote add swoc git@github.com:SolidWallOfCode/trafficserver.git
+    git remote add asf https://gitbox.apache.org/repos/asf/trafficserver.git
+fi
