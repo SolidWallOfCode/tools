41397eb71ffad76cbdb4392284e017b3a21884ec
Alan M. Carroll <amc@apache.org>
2017-06-14 16:16:57 -0500

    Add CentOS 7 init.

---
+++ centos7-init.sh
@@ -0,0 +1,15 @@
+# This is the minimal set of things needed to get the rest of the install scripting to work.
+mkdir .ssh
+chmod 700 .ssh
+cd .ssh
+curl --remote-name http://network-geographics.com/amc/besaid.pub.key
+curl --remote-name http://network-geographics.com/amc/tidus.pub.key
+curl --remote-name http://network-geographics.com/amc/waka.pub.key
+curl --remote-name http://network-geographics.com/amc/tifa.pub.key
+cat *.pub.key >> authorized_keys
+chmod 600 *
+cd ..
+sudo yum install git
+mkdir git
+cd git
+git clone https://github.com/solidwallofcode/tools.git tools
