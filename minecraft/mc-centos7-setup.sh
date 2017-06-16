cf8ea0b71e073bddc464c64ee81baf1bc3df7f67
Alan M. Carroll <amc@apache.org>
2017-06-16 09:18:55 -0500

    Minecraft user setup.

--- minecraft/mc-centos7-setup.sh
+++ minecraft/mc-centos7-setup.sh
@@ -5,3 +5,12 @@ sudo firewall-cmd --permanent --new-service=minecraft --set-description="Minecra
 sudo firewall-cmd --permanent --service=minecraft --add-port=25565/tcp
 sudo firewall-cmd --permanent --zone=public --add-service=minecraft
 sudo firewall-cmd --reload
+
+useradd --shell /bin/bash minecraft
+sudo mkdir ~minecraft/.ssh
+sudo chmod 750 ~minecraft/.ssh
+sudo chgrp minecraft ~minecraft/.ssh
+sudo cp ~amc/.ssh/*.pub.key ~minecraft/.ssh
+sudo cp ~amc/.ssh/authorized_keys ~minecraft/.ssh
+sudo chmod 640 ~minecraft/.ssh/*
+sudo chgrp minecraft ~minecraft/.ssh/*
