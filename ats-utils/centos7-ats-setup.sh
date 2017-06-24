49dc16ec267d449fc9203b52e8ded0e27784b672
Alan M. Carroll <amc@apache.org>
2017-06-24 10:28:01 -0500

    Add python-pip to centos 7 setup.

--- ats-utils/centos7-ats-setup.sh
+++ ats-utils/centos7-ats-setup.sh
@@ -2,7 +2,7 @@ sudo yum upgrade
 # For local networking, edit the connection to have a DHCP client id that
 # matches the hostname.
 #dnf install gcc gcc-c++ autoconf automake libtool gdb bison flex openssl openssl-devel tcl-devel pcre-devel hwloc-devel patch emacs wireshark-gnome libcap-devel perl-ExtUtils-MakeMaker doxygen graphviz libxml2-devel gpg libffi-devel python-devel python-virtualenv python-lxml system-config-users nghttp2 libyaml-devel redhat-rpm-config jemalloc-devel clang llvm clang-analyzer lm_sensors xfce4-sensors-plugin
-sudo yum install gcc gcc-c++ autoconf automake libtool gdb bison flex openssl-devel tcl-devel pcre-devel hwloc-devel patch emacs libcap-devel perl-ExtUtils-MakeMaker doxygen graphviz libffi-devel python-devel python-virtualenv redhat-rpm-config 
+sudo yum install gcc gcc-c++ autoconf automake libtool gdb bison flex openssl-devel tcl-devel pcre-devel hwloc-devel patch emacs libcap-devel perl-ExtUtils-MakeMaker doxygen graphviz libffi-devel python-devel python-virtualenv python-pip redhat-rpm-config 
 
 sudo pip install --upgrade pip
 sudo pip install sphinx
