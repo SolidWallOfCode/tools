07a907e010eef7df5efd7025e7a62e23e3cd8362
Alan M. Carroll <amc@apache.org>
2017-07-09 06:08:50 -0500

    Updates to debian initialization.

--- debian-init.sh
+++ debian-init.sh
@@ -1,19 +1,27 @@
+#
+# This command must be run by hand to get started.
+# wget https://raw.githubusercontent.com/SolidWallOfCode/tools/master/debian-init.sh
+#
 echo Use the root password during initial setup.
 su --command 'apt-get install sudo'
 su --command 'usermod -aG sudo amc'
-su --command ' apt-get install git'
+su --command 'apt-get install git'
 
-mkdir .ssh
-chmod 700 .ssh
-cd .ssh
-wget http://network-geographics.com/amc/besaid.pub.key
-wget http://network-geographics.com/amc/tidus.pub.key
-wget http://network-geographics.com/amc/waka.pub.key
-cat *.pub.key >> authorized_keys
-chmod 600 *
-cd ..
+cd ~
 mkdir git
 cd git
 git clone https://github.com/solidwallofcode/tools.git tools
+
+cd ~
+mkdir .ssh
+chmod 700 .ssh
+cd .ssh
+cp ~/git/tools/keys/*.pub.key .
+cat tidus.pub.key >> authorized_keys
+cat waka.pub.key >> authorized_keys
+cat tifa.pub.key >> authorized_keys
+cat besaid.pub.key >> authorized_keys
+cat spira.pub.key >> authorized_keys
+chmod 600 *
 
 echo Log out is now required to enable sudo.
