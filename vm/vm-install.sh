97393c34393631b623d6170744042a288bd5aaa4
Alan M. Carroll <amc@apache.org>
2017-07-19 19:14:22 -0500

    Add vm-install.sh

--- vm/vm-install.sh
+++ vm/vm-install.sh
@@ -1,3 +1,3 @@
-sudo dnf -y install qemu-kvm qemu-img virt-manager libvirt iptables-service
+sudo dnf -y install qemu-kvm qemu-img virt-manager libvirt
 sudo systemctl enable libvirtd
 sudo systemctl start libvirtd
