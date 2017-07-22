7a22b59a414bcfbec4db36064700f9d5124ca4e3
Alan M. Carroll <amc@apache.org>
2017-07-22 11:11:22 -0500

    Updated fedora and centos ATS setup.

---
+++ ats-utils/fedora-ats-setup.sh
@@ -0,0 +1,6 @@
+sudo dnf install gcc gcc-c++ autoconf automake libtool gdb bison flex openssl openssl-devel tcl-devel pcre-devel hwloc-devel patch emacs libcap-devel perl-ExtUtils-MakeMaker doxygen graphviz libxml2-devel libffi-devel python-devel python-virtualenv python-lxml libyaml-devel redhat-rpm-config 
+
+sudo pip install --upgrade pip
+sudo pip install sphinx
+
+# redhat-rpm-config is for installing Python pacakges via PIP that need to be compiled.
