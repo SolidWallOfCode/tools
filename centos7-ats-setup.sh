b250b25cb341f139e7e82f9a10de4935a917fb58
Alan M. Carroll <amc@apache.org>
2017-10-07 10:13:40 -0500

    ATS dev setup for Centos.

---
+++ centos7-ats-setup.sh
@@ -0,0 +1,9 @@
+sudo yum upgrade
+# For local networking, edit the connection to have a DHCP client id that
+# matches the hostname.
+sudo yum install gcc gcc-c++ autoconf automake libtool gdb bison flex openssl openssl-devel tcl-devel pcre-devel hwloc-devel patch libcap-devel perl-ExtUtils-MakeMaker doxygen graphviz libxml2-devel libffi-devel python-devel python-virtualenv python-lxml libyaml-devel redhat-rpm-config clang llvm clang-analyzer 
+
+sudo pip install --upgrade pip
+sudo pip install sphinx
+
+# redhat-rpm-config is for installing Python pacakges via PIP that need to be compiled.
