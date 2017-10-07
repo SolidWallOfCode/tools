39ec457962d71d569c5655051a87f53bc69f0dfa
Alan M. Carroll <amc@apache.org>
2017-10-07 10:49:24 -0500

    VM install fixes.

--- vm/vm-install.sh
+++ vm/vm-install.sh
@@ -1,7 +1,9 @@
-sudo dnf -y install qemu-kvm qemu-img virt-manager libvirt
+sudo yum install qemu-kvm qemu-img virt-manager libvirt
+sudo sh -c '
 if [ ! -r /etc/polkit-1/rules.d/56-libvirt.rules ] ; then
   cp 56-libvirt.rules /etc/polkit-1/rules.d
 fi
+'
 sudo systemctl enable libvirtd
 sudo systemctl start libvirtd
 
