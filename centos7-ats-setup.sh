e0a1a0e5aceaa7fb4d3c357f04060d8a87414b83
Alan M. Carroll <amc@apache.org>
2017-10-09 10:38:06 -0500

    Centos7 - pip

--- centos7-ats-setup.sh
+++ centos7-ats-setup.sh
@@ -1,7 +1,7 @@
 sudo yum upgrade
 # For local networking, edit the connection to have a DHCP client id that
 # matches the hostname.
-sudo yum install gcc gcc-c++ autoconf automake libtool gdb bison flex openssl openssl-devel tcl-devel pcre-devel hwloc-devel patch libcap-devel perl-ExtUtils-MakeMaker doxygen graphviz libxml2-devel libffi-devel python-devel python-virtualenv python-lxml libyaml-devel redhat-rpm-config clang llvm clang-analyzer 
+sudo yum install gcc gcc-c++ autoconf automake libtool gdb bison flex openssl openssl-devel tcl-devel pcre-devel hwloc-devel patch libcap-devel perl-ExtUtils-MakeMaker doxygen graphviz libxml2-devel libffi-devel python-devel python-virtualenv python-lxml libyaml-devel redhat-rpm-config clang llvm clang-analyzer python-pip
 
 sudo pip install --upgrade pip
 sudo pip install sphinx
