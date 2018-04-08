3a4058eac637e0d3940195b4fd5c67d75ef2fcf2
Alan M. Carroll <amc@apache.org>
2018-04-08 15:10:43 -0500

    Add CentOS devtoolset-7 install.

--- centos7-ats-setup.sh
+++ centos7-ats-setup.sh
@@ -3,6 +3,9 @@ sudo yum upgrade
 # matches the hostname.
 sudo yum install gcc gcc-c++ autoconf automake libtool gdb bison flex openssl openssl-devel tcl-devel pcre-devel hwloc-devel patch libcap-devel perl-ExtUtils-MakeMaker doxygen graphviz libxml2-devel libffi-devel python-devel python-virtualenv python-lxml libyaml-devel redhat-rpm-config clang llvm clang-analyzer python-pip java
 
+sudo yum install centos-release-scl
+sudo yum install install devtoolset-7
+
 sudo pip install --upgrade pip
 sudo pip install sphinx sphinx-rtd-theme sphinxcontrib-plantuml
 
