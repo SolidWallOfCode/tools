6538d9191c1b4a9f52ba03206c263b7998e8c370
Alan M. Carroll <amc@apache.org>
2018-04-08 15:02:48 -0500

    Typo.

--- centos7-ats-setup.sh
+++ centos7-ats-setup.sh
@@ -4,6 +4,6 @@ sudo yum upgrade
 sudo yum install gcc gcc-c++ autoconf automake libtool gdb bison flex openssl openssl-devel tcl-devel pcre-devel hwloc-devel patch libcap-devel perl-ExtUtils-MakeMaker doxygen graphviz libxml2-devel libffi-devel python-devel python-virtualenv python-lxml libyaml-devel redhat-rpm-config clang llvm clang-analyzer python-pip java
 
 sudo pip install --upgrade pip
-sudo pip install sphinx sphinx-rt-theme sphinxcontrib-plantuml
+sudo pip install sphinx sphinx-rtd-theme sphinxcontrib-plantuml
 
 # redhat-rpm-config is for installing Python pacakges via PIP that need to be compiled.
