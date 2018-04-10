756701d7ea392008c17c0a292ed0ecb53af4e1fc
Alan M. Carroll <amc@apache.org>
2018-04-10 13:30:43 -0500

    Updates to conntrack.py.

--- yts/conntrack.py
+++ yts/conntrack.py
@@ -10,11 +10,20 @@ args = parser.parse_args()
 with open(args.source) as stream:
     active = 0 # count of groups with non-zero counts
     total = 0 # count of open sessions in groups
+    addr = {}
+    fqdn = {}
+    a_addr = {}
+    a_fqdn = {}
     data = json.load(stream)
     data['connectionCountList'].sort(key=lambda count: count['count'])
     for group in data['connectionCountList'] :
         total += group['count']
-        active += group['count'] > 0
+        if group['count'] > 0 :
+            active += 1
+            a_addr[group['ip']] = 1
+            a_fqdn[group['fqdn']] = 1
+        addr[group['ip']] = 1
+        fqdn[group['fqdn']] = 1
         print("count {:<4} addr {:<20} block {:2} alert {}".format(group['count'], group['ip'], group['block'], group['alert']))
 
-    print("{} groups active with {} connections".format(active, total))
+    print("{} groups active with {} connections, {}/{} addresses, {}/{} fqdns".format(active, total, len(a_addr), len(addr), len(a_fqdn), len(fqdn)))
