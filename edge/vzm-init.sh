9a08efefcd4d057da719a6eddc9cb0ce15a97cff
Alan M. Carroll <amc@apache.org>
2019-07-02 17:58:42 -0500

    More tweaks for VZM VM setup.

--- edge/vzm-init.sh
+++ edge/vzm-init.sh
@@ -6,10 +6,19 @@ set -e
 sudo yum upgrade -y
 sudo yum install -y autoconf automake libtool gdb bison flex openssl openssl-devel tcl-devel expat-devel pcre-devel patch wireshark-gnome libcap-devel perl-ExtUtils-MakeMaker libunwind
 # Install EPEL
-sudo yum install -y y-rhscl-repo y-epel-release
+sudo yum install -y --enablerepo=y-extras y-rhscl-repo y-epel-release
 
 sudo yum install -y devtoolset-8-gcc-c++
+if ! grep -q devtoolset .bashrc ; then
+  echo 'source /opt/rh/devtoolset-8/enable' >> .bashrc
+fi
+source /opt/rh/devtoolset-8/enable
+
 sudo yum install -y rh-python36
+if ! grep -q rh-python36 .bashrc ; then
+  echo 'source /opt/rh/rh-python36/enable' >> .bashrc
+fi
+source /opt/rh/rh-python36/enable
 
 sudo python3 -m pip install --upgrade pip
 python3 -m pip install --user sphinx sphinx-rtd-theme sphinxcontrib-plantuml
@@ -24,7 +33,7 @@ if [ ! -d edge ] ; then
 fi
 cd edge
 if [ ! -d ts ] ; then
-  git clone git@pgit.ouroath.com:Edge/trafficserver.git ts
+  git clone git@git.ouroath.com:Edge/trafficserver.git ts
   if [ -d ts ] ; then
     cd ts
     git remote rename origin edge
@@ -34,11 +43,3 @@ if [ ! -d ts ] ; then
 fi
 
 cd
-
-if ! grep -q devtoolset .bashrc ; then
-  echo 'source /opt/rh/devtoolset-8/enable' >> .bashrc
-fi
-
-if ! grep -q rh-python36 .bashrc ; then
-  echo 'source /opt/rh/rh-python36/enable' >> .bashrc
-fi
