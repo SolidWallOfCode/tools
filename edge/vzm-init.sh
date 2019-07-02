aec14f9a5cc08a6490917d2f46e5eabffe663779
Alan M. Carroll <amc@apache.org>
2019-07-02 17:43:09 -0500

    Improve VZM open house init script.

--- edge/vzm-init.sh
+++ edge/vzm-init.sh
@@ -1,20 +1,17 @@
 ##
 # Configure a VZM OpenHouse VM to build Traffic Server.
 
+set -e
+
 sudo yum upgrade -y
 sudo yum install -y autoconf automake libtool gdb bison flex openssl openssl-devel tcl-devel expat-devel pcre-devel patch wireshark-gnome libcap-devel perl-ExtUtils-MakeMaker libunwind
 # Install EPEL
-#sudo yum install -y y-epel-release --enablerepo=y-extras y-rhscl-repo
-
-sudo yum install -y https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
-sudo yum install -y https://rhel7.iuscommunity.org/ius-release.rpm
+sudo yum install -y y-rhscl-repo y-epel-release
 
 sudo yum install -y devtoolset-8-gcc-c++
 sudo yum install -y rh-python36
-sudo yum remove -y git
-sudo yum install -y git2u-all
 
-python3 -m pip install --user --upgrade pip
+sudo python3 -m pip install --upgrade pip
 python3 -m pip install --user sphinx sphinx-rtd-theme sphinxcontrib-plantuml
 
 if [ ! -d git ] ; then
@@ -22,12 +19,16 @@ if [ ! -d git ] ; then
 fi
 
 cd git
-if [ ! -d oats ] ; then
-  git clone git@partner.git.corp.yahoo.com:Edge/trafficserver.git oats
-  if [ -d oats ] ; then
-    cd oats
+if [ ! -d edge ] ; then
+  mkdir edge
+fi
+cd edge
+if [ ! -d ts ] ; then
+  git clone git@pgit.ouroath.com:Edge/trafficserver.git ts
+  if [ -d ts ] ; then
+    cd ts
     git remote rename origin edge
-    git remote add edge-swoc git@partner.git.corp.yahoo.com:SolidWallOfCode/trafficserver.git
+    git remote add edge-swoc git@git.ouroath.com:SolidWallOfCode/trafficserver.git
     git remote add ats https://github.com/apache/trafficserver/trafficserver.git
   fi
 fi
